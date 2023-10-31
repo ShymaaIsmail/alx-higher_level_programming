d32d2582643222eef149f9cda365ae53d12223c5
shymaaismail <shymaa.m.ismail@gmail.com>
2023-11-01 01:11:21 +0300

    pow

--- 0x01-python-if_else_loops_functions/11-pow.py
+++ 0x01-python-if_else_loops_functions/11-pow.py
@@ -1,3 +1,10 @@
 #!/usr/bin/python3
 def pow(a, b):
-    return a ^ b
+    value = 0
+    if b < 0:
+        value = -1
+    elif b == 0:
+        value = 1
+    else:
+        value = a * (pow(a, b - 1))
+    return value

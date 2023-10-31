8f21900f2ce051632f3e993fa4d21a4d3afcd37a
shymaaismail <shymaa.m.ismail@gmail.com>
2023-11-01 01:14:02 +0300

    pow

--- 0x01-python-if_else_loops_functions/11-pow.py
+++ 0x01-python-if_else_loops_functions/11-pow.py
@@ -2,9 +2,9 @@
 def pow(a, b):
     value = 0
     if b < 0:
-        value = -1
+       value = 1 / pow(a, -b)
     elif b == 0:
         value = 1
     else:
-        value = a * (pow(a, b - 1))
+        value = a * pow(a, b - 1)
     return value

32d1319524020a73b9f91f001ab4cea8065d04f0
shymaaismail <shymaa.m.ismail@gmail.com>
2023-11-03 02:37:58 +0300

    args

--- 0x02-python-import_modules/2-args.py
+++ 0x02-python-import_modules/2-args.py
@@ -1,9 +1,10 @@
 #!/usr/bin/python3
 import sys
-args = len(sys.argv) - 1
-if args == 1:
-    print("{:d} argument.".format(args))
-else:
-    print("{:d} arguments.".format(args))
-for i in range(1, args):
-    print("{}: {}".format(i, sys.argv[i]))
+if __name__ == "__main__":
+    args = len(sys.argv) - 1
+    if args == 1:
+        print("{:d} argument.".format(args))
+    else:
+        print("{:d} arguments.".format(args))
+    for i in range(1, args):
+        print("{}: {}".format(i, sys.argv[i]))

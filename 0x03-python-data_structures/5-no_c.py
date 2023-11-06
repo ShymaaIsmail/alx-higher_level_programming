15b8ff47acb290c075222811eacbb3c0a4332921
shymaaismail <shymaa.m.ismail@gmail.com>
2023-11-06 21:37:27 +0300

    s

--- 0x03-python-data_structures/5-no_c.py
+++ 0x03-python-data_structures/5-no_c.py
@@ -1,6 +1,6 @@
 #!/usr/bin/python3
 def no_c(my_string):
-    new_string = my_string
+    new_string = ""
     for index in range(0, len(my_string)):
         if my_string[index] != 'c' and my_string[index] != 'C':
             new_string += my_string[index]

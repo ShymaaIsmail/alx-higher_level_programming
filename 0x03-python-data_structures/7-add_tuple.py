cbb3685eee4a20d9baeb5249f002a3b525912819
shymaaismail <shymaa.m.ismail@gmail.com>
2023-11-07 00:44:36 +0300

    tuple

--- 0x03-python-data_structures/7-add_tuple.py
+++ 0x03-python-data_structures/7-add_tuple.py
@@ -2,7 +2,7 @@
 def add_tuple(tuple_a=(), tuple_b=()):
     temp_tuple = (0, 0)
     if len(tuple_a) < 2:
-        tuple_a += temp_tuple
+        tuple_a += temp_tuple[len(tuple_a):]
     elif len(tuple_b) < 2:
-        tuple_b += temp_tuple
+        tuple_b += temp_tuple[len(tuple_b):]
     return (tuple_a[0] + tuple_b[0], tuple_a[1] + tuple_b[1])

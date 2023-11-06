ca500765021c0115fb057551a3ac363b07d10f3f
shymaaismail <shymaa.m.ismail@gmail.com>
2023-11-07 01:46:41 +0300

    div 2

---
+++ 0x03-python-data_structures/10-divisible_by_2.py
@@ -0,0 +1,14 @@
+#!/usr/bin/python3
+def divisible_by_2(my_list=[]):
+    if my_list and len(my_list) > 0:
+        bool_list = []
+        idx = 0
+        for i in my_list:
+            if i % 2 == 0:
+                bool_list[idx] = True
+            else:
+                bool_list[idx] = False
+            idx += 1
+        return bool_list
+    else:
+        return my_list

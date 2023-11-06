c0f32cc9060b11c00910aec3d3d1bc020312eb5d
shymaaismail <shymaa.m.ismail@gmail.com>
2023-11-07 01:55:22 +0300

    delete

---
+++ 0x03-python-data_structures/11-delete_at.py
@@ -0,0 +1,10 @@
+#!/usr/bin/python3
+def delete_at(my_list=[], idx=0):
+    if idx >= 0 and idx < len(my_list):
+        new_list = []
+        for i in range(0, len(my_list)):
+            if i != idx:
+                new_list.append(my_list[i])
+        return new_list
+    else:
+        return my_list

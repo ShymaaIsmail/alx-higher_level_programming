8ca993c8b1a820d96e386665801664a52869a18d
shymaaismail <shymaa.m.ismail@gmail.com>
2023-11-07 01:49:10 +0300

    div 2

--- 0x03-python-data_structures/10-divisible_by_2.py
+++ 0x03-python-data_structures/10-divisible_by_2.py
@@ -2,13 +2,11 @@
 def divisible_by_2(my_list=[]):
     if my_list and len(my_list) > 0:
         bool_list = []
-        idx = 0
         for i in my_list:
             if i % 2 == 0:
-                bool_list[idx] = True
+                bool_list.append(True)
             else:
-                bool_list[idx] = False
-        idx += 1
+                bool_list.append(False)
         return bool_list
     else:
         return my_list

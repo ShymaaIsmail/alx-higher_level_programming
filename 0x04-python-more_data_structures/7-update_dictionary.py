2ab41f672129653746b4911aa4dda53a0d33edb2
shymaaismail <shymaa.m.ismail@gmail.com>
2023-11-09 02:05:26 +0300

    up

--- 0x04-python-more_data_structures/7-update_dictionary.py
+++ 0x04-python-more_data_structures/7-update_dictionary.py
@@ -1,9 +1,9 @@
 #!/usr/bin/python3
 def update_dictionary(a_dictionary, key, value):
     new_dict = dict(a_dictionary)
-    existing_item = dict(filter(lambda k: k == key, a_dictionary.items()))
-    if len(existing_item) > 0:
-        new_dict.update(key, value)
-    else:
+    if key in new_dict:
         new_dict[key] = value
+    else:
+        new_dict.update({key: value})
+
     return new_dict

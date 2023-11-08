a79df8965399bba1cab2fb494a6d28f446d3c7ef
shymaaismail <shymaa.m.ismail@gmail.com>
2023-11-09 02:34:35 +0300

    max best

--- 0x04-python-more_data_structures/10-best_score.py
+++ 0x04-python-more_data_structures/10-best_score.py
@@ -2,8 +2,9 @@
 def best_score(a_dictionary):
     max_score = 0
     key = None
-    for k, v in a_dictionary.items():
-        if v > max_score:
-            max_score = v
-            key = k
+    if a_dictionary:
+        for k, v in a_dictionary.items():
+            if v > max_score:
+                max_score = v
+                key = k
     return key

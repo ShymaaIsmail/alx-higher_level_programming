639f04a29702635d6e5cdb12fd23008ed2b0b592
shymaaismail <shymaa.m.ismail@gmail.com>
2023-11-09 03:00:36 +0300

    roman

---
+++ 0x04-python-more_data_structures/12-roman_to_int.py
@@ -0,0 +1,8 @@
+#!/usr/bin/python3
+def roman_to_int(roman_string):
+    number = 0
+    roman_symbol = { "I": 1, "V": 5, "X": 10, "L": 50, "C": 100, "D": 500, "M": 1000}
+    if roman_string is not None and isinstance(roman_string, str):
+        for c in roman_string:
+            number += roman_symbol.get(c)
+    return number

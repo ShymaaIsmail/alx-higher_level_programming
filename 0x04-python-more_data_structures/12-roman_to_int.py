8fe37f2d18587aa7567109465dc8ba153ae8d06a
shymaaismail <shymaa.m.ismail@gmail.com>
2023-11-09 03:11:09 +0300

    fix

--- 0x04-python-more_data_structures/12-roman_to_int.py
+++ 0x04-python-more_data_structures/12-roman_to_int.py
@@ -4,6 +4,11 @@ def roman_to_int(roman_string):
     roman_symbol = {"I": 1, "V": 5, "X": 10,
                     "L": 50, "C": 100, "D": 500, "M": 1000}
     if roman_string is not None and isinstance(roman_string, str):
-        for c in roman_string:
-            number += roman_symbol.get(c)
+        for i in range(len(roman_string)):
+            c = roman_string[i]
+            prev_c = roman_string[i - 1]
+            if i > 0 and roman_symbol[c] > roman_symbol[prev_c]:
+                number += roman_symbol[c] - 2 * roman_symbol[prev_c]
+            else:
+                number += roman_symbol[c]
     return number

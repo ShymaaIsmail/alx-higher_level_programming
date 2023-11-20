6f0c0787635af0ad5123b28a1ddab46c3ebfacac
ShymaaIsmail <shymaa.m.ismail@gmail.com>
2023-11-21 01:53:34 +0200

    Update 0-safe_print_list.py

--- 0x05-python-exceptions/0-safe_print_list.py
+++ 0x05-python-exceptions/0-safe_print_list.py
@@ -2,7 +2,7 @@
 def safe_print_list(my_list=[], x=0):
 	try:
         count = 0
-		for element in my_list:
+	for element in my_list:
             print(element, end='')
             count += 1
             if count == x:
@@ -11,4 +11,4 @@ def safe_print_list(my_list=[], x=0):
         pass
     finally:
         print()
-    return count
+        return count

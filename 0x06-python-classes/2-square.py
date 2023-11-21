17a52cf6ad217ab9a98693443c62bd3ac3ec5d52
shymaaismail <shymaa.m.ismail@gmail.com>
2023-11-22 02:51:42 +0300

    task 2

---
+++ 0x06-python-classes/2-square.py
@@ -0,0 +1,15 @@
+#!/usr/bin/python3
+"""Square class summary
+"""
+
+
+class Square:
+    """square constructor
+    """
+
+    def __init__(self, size=0):
+        if not isinstance(size, int):
+            raise TypeError("size must be an integer")
+        if size < 0:
+            raise ValueError("size must be >= 0")
+        self._Square__size = size

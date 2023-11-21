534b1029ddcd7ee88a6cffa955c0537cfaf859c9
shymaaismail <shymaa.m.ismail@gmail.com>
2023-11-21 23:54:03 +0300

    in progress

--- 0x00-python-hello_world/10-check_cycle.c
+++ 0x00-python-hello_world/10-check_cycle.c
@@ -1,8 +1,37 @@
+#include "lists.h"
+
+/**
+ * print_listint - print_listint
+ * @h: linked list
+ * Return: int
+*/
+size_t print_listint(const listint_t *h)
+{
+
+}
+
+/**
+ * add_nodeint- add_nodeint
+*/
+listint_t *add_nodeint(listint_t **head, const int n);
+
+/**
+ * free_listint - free_listint
+*/
+void free_listint(listint_t *head);
+
+
+/**
+* check_cycle - check_cycle
+* @list: list
+* Return: 0 if there is no cycle, 1 if there is a cycle
+*/
 int check_cycle(listint_t *list)
 {
-int checker = 0;
-if (list != NULL)
-{
-}
-return (checker);
+	int checker = 0;
+
+	if (list != NULL)
+	{
+	}
+	return (checker);
 }

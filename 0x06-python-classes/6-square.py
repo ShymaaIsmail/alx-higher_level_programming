91fcba9f77993b42447d91cb1456a95ea3404332
shymaaismail <shymaa.m.ismail@gmail.com>
2023-11-22 03:48:33 +0300

    task 6

--- 0x06-python-classes/6-square.py
+++ 0x06-python-classes/6-square.py
@@ -43,10 +43,10 @@ class Square:
         if self.__size == 0:
             print()
         else:
-            for i in range(self.__position[1]):
+            for i in range(self._Square__position[1]):
                 print()
             for i in range(self.__size):
-                for p in range(self.position[0]):
+                for p in range(self._Square__position[0]):
                     print(" ", end="")
                 for j in range(self.__size):
                     print("#", end="")

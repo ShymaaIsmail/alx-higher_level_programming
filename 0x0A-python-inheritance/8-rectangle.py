ef72f99d1059b230e3d20c6504674311582d8e1f
shymaaismail <shymaa.m.ismail@gmail.com>
2023-12-05 00:29:59 +0300

    task 8

---
+++ 0x0A-python-inheritance/8-rectangle.py
@@ -0,0 +1,32 @@
+#!/usr/bin/python3
+BaseGeometry = __import__('7-base_geometry').BaseGeometry
+"""
+This is 8-rectangle
+doc
+task
+number 8
+"""
+
+
+class Rectangle(BaseGeometry):
+    """
+    Rectangle
+    sub Class
+    doc
+
+    """
+
+    def __init__(self, width, height):
+        """_summary_
+
+        Args:
+            width (_type_): _description_
+            height (_type_): _description_
+        """
+        try:
+            self.integer_validator("width", width)
+            self.integer_validator("height", height)
+            self.__width = width
+            self.__height = height
+        except (ValueError, TypeError) as e:
+            raise e

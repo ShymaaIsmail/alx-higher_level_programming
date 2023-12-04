16594097faa0b51b06eb2d6fd81e62be66c12143
shymaaismail <shymaa.m.ismail@gmail.com>
2023-12-04 17:51:22 +0300

    task 4

---
+++ 0x0A-python-inheritance/4-inherits_from.py
@@ -0,0 +1,16 @@
+#!/usr/bin/python3
+"""
+This is
+4-inherits_from
+module
+"""
+
+
+def inherits_from(obj, a_class):
+    """ inherits_from
+
+    Args:
+        obj (_type_): _description_
+        a_class (_type_): _description_
+    """
+    return issubclass(obj, a_class)

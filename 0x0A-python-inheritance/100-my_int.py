6b69f1f21693b76afca5f4148fbea3d3cb042c49
shymaaismail <shymaa.m.ismail@gmail.com>
2023-12-05 01:35:50 +0300

    first advanced

---
+++ 0x0A-python-inheritance/100-my_int.py
@@ -0,0 +1,25 @@
+#!/usr/bin/python3
+"""
+This is 100-my_int
+MyInt  class doc
+for task 100
+"""
+
+
+class MyInt (int):
+    """_summary_
+
+    Args:
+        int (_type_): _description_
+    """
+    def __eq__(self, other):
+        """
+        revert eq
+        """
+        return self != other
+
+    def __ne__(self, other):
+        """
+        revert eq
+        """
+        return not self.__eq__

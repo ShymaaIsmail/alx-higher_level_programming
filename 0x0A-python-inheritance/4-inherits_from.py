b041653bea569e9ff255d93949c7dc0c08b9fdbc
shymaaismail <shymaa.m.ismail@gmail.com>
2023-12-04 18:04:40 +0300

    task 4

--- 0x0A-python-inheritance/4-inherits_from.py
+++ 0x0A-python-inheritance/4-inherits_from.py
@@ -13,4 +13,4 @@ def inherits_from(obj, a_class):
         obj (_type_): _description_
         a_class (_type_): _description_
     """
-    return issubclass(type(obj), a_class)
+    return issubclass(type(obj), a_class) and type(obj) != a_class

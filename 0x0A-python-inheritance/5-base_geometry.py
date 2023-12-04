9e229573f6f0f04173250c75a2c75a8fda76a8ac
shymaaismail <shymaa.m.ismail@gmail.com>
2023-12-04 18:09:01 +0300

    task 5

---
+++ 0x0A-python-inheritance/5-base_geometry.py
@@ -0,0 +1,21 @@
+#!/usr/bin/python3
+"""
+This is 5-base_geometry
+module
+doc
+"""
+
+
+class BaseGeometry:
+    """
+    BaseGeometry
+    Base Class
+    """
+
+def __init__():
+    """
+    BaseGeometry
+    init function
+
+    """
+    pass

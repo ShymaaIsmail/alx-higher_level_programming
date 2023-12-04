e609f60b52e3a7a1ed313a7ce7ec79d220635ef3
shymaaismail <shymaa.m.ismail@gmail.com>
2023-12-05 01:49:41 +0300

    first advanced

--- 0x0A-python-inheritance/101-add_attribute.py
+++ 0x0A-python-inheritance/101-add_attribute.py
@@ -1,8 +1,12 @@
 #!/usr/bin/python3
 """_summary_
+
+last advanced task
 """
+
+
 def add_attribute(a_class, attr, value):
     """Add an attribute to a class."""
     setattr(a_class, attr, value)
-    if not getattr(a_class,attr):
+    if not getattr(a_class, attr):
         raise Exception("can't add new attribute")

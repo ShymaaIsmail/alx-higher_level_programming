99af4f3ed6d14e93ac65e847e2865a73beda245b
shymaaismail <shymaa.m.ismail@gmail.com>
2023-12-04 22:51:39 +0300

    task 7

--- 0x0A-python-inheritance/7-base_geometry.py
+++ 0x0A-python-inheritance/7-base_geometry.py
@@ -1,46 +1,35 @@
 #!/usr/bin/python3
 """
-This is 7-base_geometry
-doc
-task
-number 7
+Module for basic class implementation
 """
 
 
 class BaseGeometry:
+    """Basic class named BaseGeometry
     """
-    BaseGeometry
-    Base Class
-    doc
-
-    
-    """
-
-    def __init__(self):
-        """
-        BaseGeometry
-        init function
-        doc
-        """
-        pass
 
     def area(self):
         """
-        area function
-        for geometery
-        calculation
+        Public instance method: raises exception
+        No implementation yet
+        Exception: area() is not implemented
         """
         raise Exception("area() is not implemented")
 
     def integer_validator(self, name, value):
-        """_summary_
-        validates value:
-        you can assume name is always a string
-        Args:
-            name (_type_): _description_
-            value (_type_): _description_
         """
-        if type(value) is not int:
-            raise TypeError(f"{name} must be an integer")
+        public instance method: validate value
+        Args :
+            name and value
+        Raises :
+            if value is not an integer: TypeError exception,
+            with the message <name> must be an integer
+            if value is less or equal to 0: raise a ValueError
+            exception with the message <name> must be greater than 0
+        """
+
+        if type(value) != int:
+            raise TypeError("{} must be an integer".format(name))
+
         if value <= 0:
-            raise ValueError(f"{name} must be greater than 0")
+            raise ValueError("{} must be greater than 0".format(name))

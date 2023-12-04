9f498d085df2d0f06ac594151d7f74ac601cc7c4
shymaaismail <shymaa.m.ismail@gmail.com>
2023-12-04 17:16:57 +0300

    mylist

--- 0x0A-python-inheritance/1-main.py
+++ 0x0A-python-inheritance/1-main.py
@@ -1,12 +1,7 @@
 #!/usr/bin/python3
 MyList = __import__('1-my_list').MyList
 
-my_list = list()
-my_list.append(1)
-my_list.append(4)
-my_list.append(2)
-my_list.append(3)
-my_list.append(int('inf'))
+my_list = list([9, int('inf')]) 
 print(my_list)
 my_list.sort()
 print(my_list)

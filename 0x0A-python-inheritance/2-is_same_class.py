2a2d17277a126652569e796c00387b04bcbef346
shymaaismail <shymaa.m.ismail@gmail.com>
2023-12-04 17:21:04 +0300

    task 2

---
+++ 0x0A-python-inheritance/2-is_same_class.py
@@ -0,0 +1,11 @@
+#!/usr/bin/python3
+"""2-is_same_class module
+"""
+def is_same_class(obj, a_class):
+    """is_same_class
+
+    Args:
+        obj (_type_): _description_
+        a_class (_type_): _description_
+    """
+    return isinstance(obj, a_class) 

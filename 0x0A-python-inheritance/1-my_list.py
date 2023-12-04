21b1fa61db4fb2c889bcaac2496e066bb16dc17d
ShymaaIsmail <shymaa.m.ismail@gmail.com>
2023-12-04 07:50:39 +0300

    Update 1-my_list.py

--- 0x0A-python-inheritance/1-my_list.py
+++ 0x0A-python-inheritance/1-my_list.py
@@ -1,7 +1,7 @@
 #!/usr/bin/python3
 class MyList(List):
   def __init__(self):
-    super.__init__(self)
+    super().__init__(self)
   def print_sorted(self):
-    super.sort()
-    print(super.items)
+    super().sort(self)
+    print(super().items)

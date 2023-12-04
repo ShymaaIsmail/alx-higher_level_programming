9cebd840635972bcc804fa71c15ae80ef44d5017
ShymaaIsmail <shymaa.m.ismail@gmail.com>
2023-12-04 12:20:15 +0300

    Update 0-lookup.py

--- 0x0A-python-inheritance/0-lookup.py
+++ 0x0A-python-inheritance/0-lookup.py
@@ -1,5 +1,14 @@
 #!/usr/bin/python3
+"""
+0-lookup.py
+
+summary
+"""
 def lookup(obj):
+  """lookup function
+  to list all attrs and methods 
+  in an obj
+  """
   list = []
   if obj is not None:
     list = dir(obj)

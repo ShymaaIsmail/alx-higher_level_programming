2a2200e3c5ecaf9c4793d238e63fbf21acb59055
shymaaismail <shymaa.m.ismail@gmail.com>
2023-12-05 21:00:05 +0300

    to json

---
+++ 0x0B-python-input_output/3-to_json_string.py
@@ -0,0 +1,18 @@
+#!/usr/bin/python3
+import json
+"""
+3-to_json_string module
+
+"""
+
+
+def to_json_string(my_obj):
+    """_summary_
+
+    Args:
+        my_obj (_type_): _description_
+
+    Returns:
+        _type_: _description_
+    """
+    return json.dumps(my_obj)

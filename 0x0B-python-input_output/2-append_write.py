501235cfab89c7c9d8209adb1ac9760a5fc8cc1c
shymaaismail <shymaa.m.ismail@gmail.com>
2023-12-05 17:30:13 +0300

    task 2

---
+++ 0x0B-python-input_output/2-append_write.py
@@ -0,0 +1,17 @@
+#!/usr/bin/python3
+"""
+2-append_write module for task 2
+This is the full doc
+for this task
+"""
+
+
+def append_write(filename="", text=""):
+    """append_write
+
+    Args:
+        filename (str, optional): _description_. Defaults to "".
+        text (str, optional): _description_. Defaults to "".
+    """
+    with open(filename, 'w+a') as file:
+        return file.write(text)

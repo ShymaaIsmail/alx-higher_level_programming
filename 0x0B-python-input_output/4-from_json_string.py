9ac40e4beb3dcabaa4413e53052e608b4316d145
shymaaismail <shymaa.m.ismail@gmail.com>
2023-12-05 21:11:16 +0300

    from json

---
+++ 0x0B-python-input_output/4-from_json_string.py
@@ -0,0 +1,18 @@
+#!/usr/bin/python3
+"""
+4-from_json_string module
+
+"""
+import json
+
+
+def from_json_string(my_obj):
+    """_summary_
+
+    Args:
+        my_obj (_type_): _description_
+
+    Returns:
+        _type_: _description_
+    """
+    return json.loads(my_obj)

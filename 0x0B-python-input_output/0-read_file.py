021eb3dc1a665e7dbe272a6ef0e1e23b8a39f678
shymaaismail <shymaa.m.ismail@gmail.com>
2023-12-05 17:02:02 +0300

    task 0

--- 0x0B-python-input_output/0-read_file.py
+++ 0x0B-python-input_output/0-read_file.py
@@ -1,15 +1,18 @@
 #!/usr/bin/python3
 """
 0-read_file module for task 0
-This is the full doc 
+This is the full doc
 for this task
 """
+
+
 def read_file(filename=""):
     """read_file
 
     Args:
         filename (str, optional): _description_. Defaults to "".
     """
-    with open(filename, "r", encoding="utf-8") as f:
-        read_data = f.read()
-        print(f'{read_data}')
+    if filename is not None:
+        with open(filename, "r", encoding="utf-8") as f:
+            read_data = f.read()
+            print(f'{read_data}')

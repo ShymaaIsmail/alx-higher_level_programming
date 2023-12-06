b1db9b7f8f6ae7bcd6b198c18f5ffe27ae075e40
shymaaismail <shymaa.m.ismail@gmail.com>
2023-12-06 07:54:31 +0300

    task8

--- 0x0B-python-input_output/8-class_to_json.py
+++ 0x0B-python-input_output/8-class_to_json.py
@@ -3,5 +3,16 @@
 8-class_to_json module
 
 """
+
+
 def class_to_json(obj):
-    
+    """class_to_json
+
+    Args:
+        obj (_type_): _description_
+    """
+    result = {}
+    for attr, value in obj.__dict__.items():
+        if isinstance(value, (list, dict, str, int, bool)):
+            result[attr] = value
+    return result

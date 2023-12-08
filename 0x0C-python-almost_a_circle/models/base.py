78487741e3bce8126187ed5824b0fa2cc56514f4
shymaaismail <shymaa.m.ismail@gmail.com>
2023-12-09 02:09:22 +0300

    task3

--- 0x0C-python-almost_a_circle/models/base.py
+++ 0x0C-python-almost_a_circle/models/base.py
@@ -1,17 +1,19 @@
 #!/usr/bin/python3
 
 """base clase
+module do
 """
+
+
 class Base:
     """Base Class
     """
     __nb_objects = 0
     id = 0
+
     def __init__(self, id=None):
         if id is not None:
             self.id = id
         else:
             Base.__nb_objects += 1
             self.id = Base.__nb_objects
-            
-
---
+++ 0x0C-python-almost_a_circle/models/rectangle.py
@@ -0,0 +1,54 @@
+#!/usr/bin/python3
+"""
+Rectangle Class
+"""
+from models.base import Base
+
+
+class Rectangle(Base):
+    """Rectangle Class
+    """
+    __width = 0
+    __height = 0
+    __x = 0
+    __y = 0
+
+    @property
+    def width(self):
+        return self.__width
+
+    @width.setter
+    def width(self, width):
+        self.__width = width
+
+    @property
+    def height(self):
+        return self.__height
+
+    @height.setter
+    def height(self, height):
+        self.__height = height
+
+    @property
+    def x(self):
+        return self.__x
+
+    @x.setter
+    def x(self, x):
+        self.__x = x
+
+    @property
+    def y(self):
+        return self.__y
+
+    @y.setter
+    def y(self, y):
+        self.__y = y
+
+    def __init__(self, width, height, x=0, y=0, id=None):
+        """Initialize Rectangle"""
+        super().__init__(id)
+        self.__width = width
+        self.__height = height
+        self.__x = x
+        self.__y = y

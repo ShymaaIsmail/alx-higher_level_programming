982b62587a9c4698a4b6d0e7959f7e578d7b06a6
shymaaismail <shymaa.m.ismail@gmail.com>
2023-12-09 02:37:28 +0300

    task 3

--- 0x0C-python-almost_a_circle/models/rectangle.py
+++ 0x0C-python-almost_a_circle/models/rectangle.py
@@ -19,6 +19,8 @@ class Rectangle(Base):
 
     @width.setter
     def width(self, width):
+        self.__validate_int("width", width)
+        self.__validate_positive("width", width)
         self.__width = width
 
     @property
@@ -27,6 +29,8 @@ class Rectangle(Base):
 
     @height.setter
     def height(self, height):
+        self.__validate_int("height", height)
+        self.__validate_positive("height", height)
         self.__height = height
 
     @property
@@ -35,6 +39,8 @@ class Rectangle(Base):
 
     @x.setter
     def x(self, x):
+        self.__validate_int("x", x)
+        self.__validate_negative("x", x)
         self.__x = x
 
     @property
@@ -43,12 +49,51 @@ class Rectangle(Base):
 
     @y.setter
     def y(self, y):
+        self.__validate_int("y", y)
+        self.__validate_negative("y", y)
         self.__y = y
 
     def __init__(self, width, height, x=0, y=0, id=None):
         """Initialize Rectangle"""
+        self.__validate(width, height, x, y)
         super().__init__(id)
         self.__width = width
         self.__height = height
         self.__x = x
         self.__y = y
+
+    def __validate(self, width, height, x, y):
+        self.__validate_int("width", width)
+        self.__validate_positive("width", width)
+        self.__validate_int("height", height)
+        self.__validate_positive("height", height)
+        self.__validate_int("x", x)
+        self.__validate_negative("x", x)
+        self.__validate_int("y", y)
+        self.__validate_negative("y", y)
+
+    def __validate_int(self, name, value):
+        """
+        __validate_int
+        validates value:
+        you can assume name is always a string
+        Args:
+            name (_type_): _description_
+            value (_type_): _description_
+        """
+        if value.__class__ != int:
+            raise TypeError(f"{name} must be an integer")
+
+    def __validate_negative(self, name, value):
+        """
+        __validate_negative
+        """
+        if value < 0:
+            raise ValueError(f"{name} must be >= 0")
+
+    def __validate_positive(self, name, value):
+        """
+        __validate_positive
+        """
+        if value <= 0:
+            raise ValueError(f"{name} must be > 0")

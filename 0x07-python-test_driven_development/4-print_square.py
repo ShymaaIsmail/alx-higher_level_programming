e8682fa95513d6850c806f9f889716b9e7abda8b
shymaaismail <shymaa.m.ismail@gmail.com>
2023-12-08 21:33:41 +0300

    say my name

--- 0x07-python-test_driven_development/4-print_square.py
+++ 0x07-python-test_driven_development/4-print_square.py
@@ -2,19 +2,21 @@
 """
 4-print_square
 """
+
+
 def print_square(size):
     """_summary_
 
     Args:
         size (_type_): _description_
     """
-    if (type(size) == float and size < 0):
+    if (isinstance(size, float) and size < 0):
         raise TypeError("size must be an integer")
-    if (type(size) != int and type(size) != float):
+    if (not isinstance(size, int) and not isinstance(size, float)):
         raise TypeError("size must be an integer")
     if size < 0:
         raise ValueError("size must be >= 0")
     for row in range(0, size):
         for column in range(0, size):
-            print("#", end=" ")
-    print()
+            print("#", end="")
+        print()

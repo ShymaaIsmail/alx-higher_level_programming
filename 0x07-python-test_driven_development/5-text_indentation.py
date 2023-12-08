13035939efcdb3b5969e3d2615fa8fa1045a01ff
shymaaismail <shymaa.m.ismail@gmail.com>
2023-12-08 22:02:08 +0300

    text indentation

--- 0x07-python-test_driven_development/5-text_indentation.py
+++ 0x07-python-test_driven_development/5-text_indentation.py
@@ -1,1 +1,22 @@
 #!/usr/bin/python3
+"""
+5-text_indentation module
+"""
+
+
+def text_indentation(text):
+    """text_indentation
+
+    Args:
+        text (_type_): _description_
+    """
+    if not isinstance(text, str):
+        raise TypeError("text must be a string")
+    delimiters = [":", "?", "."]
+    text = text.strip()
+    for char in text:
+        print(char, end="")
+        if char in delimiters:
+            print("\n\n", end="")
+    if text and text[-1] not in delimiters:
+        print("\n")

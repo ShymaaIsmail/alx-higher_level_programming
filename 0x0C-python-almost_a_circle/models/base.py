5d2e386c5d7d581d049f3e372a0ba839fa3d5e6b
shymaaismail <shymaa.m.ismail@gmail.com>
2023-12-11 00:58:04 +0300

    task 15

--- 0x0C-python-almost_a_circle/models/base.py
+++ 0x0C-python-almost_a_circle/models/base.py
@@ -3,6 +3,7 @@
 """base clase
 module do
 """
+import json
 
 
 class Base:
@@ -12,8 +13,25 @@ class Base:
     id = 0
 
     def __init__(self, id=None):
+        """_summary_
+
+        Args:
+            id (_type_, optional): _description_. Defaults to None.
+        """
         if id is not None:
             self.id = id
         else:
             Base.__nb_objects += 1
             self.id = Base.__nb_objects
+
+    @staticmethod
+    def to_json_string(list_dictionaries):
+        """_summary_
+
+        Args:
+            list_dictionaries (_type_): _description_
+        """
+        if (list_dictionaries is None) or len(list_dictionaries) == 0:
+            return "[]"
+        else:
+            return json.dumps(list_dictionaries)

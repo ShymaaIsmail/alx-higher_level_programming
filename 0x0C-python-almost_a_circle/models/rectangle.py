2cb8b18dd3cdc7aa41b2f93f2601a795c6a3901a
shymaaismail <shymaa.m.ismail@gmail.com>
2023-12-12 00:02:41 +0300

    temp change

--- 0x0C-python-almost_a_circle/models/rectangle.py
+++ 0x0C-python-almost_a_circle/models/rectangle.py
@@ -106,20 +106,30 @@ class Rectangle(Base):
         self.__y = y
 
     def __init__(self, width, height, x=0, y=0, id=None):
-        """_summary_
-
-        Args:
-            width (_type_): _description_
-            height (_type_): _description_
-            x (int, optional): _description_. Defaults to 0.
-            y (int, optional): _description_. Defaults to 0.
-            id (_type_, optional): _description_. Defaults to None.
-        """
+        """ constructor to set attributes upon instantiation"""
         super().__init__(id)
-        self.__validate(width, height, x, y)
+        if type(width) != int:
+            raise TypeError("width must be an integer")
+        if width < 0:
+            raise ValueError("width must be greater than zero!")
         self.__width = width
+
+        if type(height) != int:
+            raise TypeError("height must be an integer")
+        if height < 0:
+            raise ValueError("height must be greater than zero!")
         self.__height = height
+
+        if type(x) != int:
+            raise TypeError("x must be an integer")
+        if x < 0:
+            raise ValueError("x must be greater than zero!")
         self.__x = x
+
+        if type(y) != int:
+            raise TypeError("y must be an integer")
+        if y < 0:
+            raise ValueError("y must be greater than zero!")
         self.__y = y
 
     def __validate(self, width, height, x, y):

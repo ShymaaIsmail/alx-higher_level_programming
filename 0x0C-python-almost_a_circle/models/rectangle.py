20aef53a50a7ba8fe2502d6be6488269f9251b2a
shymaaismail <shymaa.m.ismail@gmail.com>
2023-12-11 23:51:31 +0300

    temp change

--- 0x0C-python-almost_a_circle/models/rectangle.py
+++ 0x0C-python-almost_a_circle/models/rectangle.py
@@ -1,159 +1,202 @@
 #!/usr/bin/python3
 
-"""This module is about a class Rectangle that inherits from Base
 """
-
+models.rectangle module
+Rectangle Class
+very long text
+"""
 from models.base import Base
 
 
+"""
+models.rectangle module
+Rectangle Class
+very long text
+"""
+
+
 class Rectangle(Base):
-    """class with private attributes
+    """Rectangle class with private attributes
     Args: width, height, x, y
     Raises : TypeError, ValueError
     """
 
-    def __init__(self, width, height, x=0, y=0, id=None):
-        """ constructor to set attributes upon instantiation"""
-        super().__init__(id)
-        if type(width) != int:
-            raise TypeError("width must be an integer")
-        if width < 0:
-            raise ValueError("width must be greater than zero!")
-        self.__width = width
-
-        if type(height) != int:
-            raise TypeError("height must be an integer")
-        if height < 0:
-            raise ValueError("height must be greater than zero!")
-        self.__height = height
-
-        if type(x) != int:
-            raise TypeError("x must be an integer")
-        if x < 0:
-            raise ValueError("x must be greater than zero!")
-        self.__x = x
-
-        if type(y) != int:
-            raise TypeError("y must be an integer")
-        if y < 0:
-            raise ValueError("y must be greater than zero!")
-        self.__y = y
-
-    def area(self):
-        """function to calc area of rectangle
-
-        Returns:
-            int: area
-        """
-        return self.__width * self.__height
-
-    def display(self):
-        """display rectangle with "#"
-
-        Returns:
-           print "#" according to w and h_
-        """
-
-        for y_val in range(self.__y):
-            print()
-
-        for i in range(self.__height):
-            for x_val in range(self.__x):
-                print(" ", end="")
-            print("#" * self.__width)
-
-    def update(self, *args, **kwargs):
-        """assigns an argument to each attribute
-        """
-        if args and len(args) != 0:
-
-            for arg in args:
-
-                if len(args) >= 1:
-                    self.id = args[0]
-                if len(args) >= 2:
-                    self.width = args[1]
-                if len(args) >= 3:
-                    self.height = args[2]
-                if len(args) >= 4:
-                    self.x = args[3]
-                if len(args) >= 5:
-                    self.y = args[4]
-        else:
-            for k, v in kwargs.items():
-                setattr(self, k, v)
-
-    def __str__(self):
+    @property
+    def width(self):
         """_summary_
 
         Returns:
-            string representation [Rectangle] (<id>) <x>/<y> - <width>/<height>
+            _type_: _description_
         """
-        return f"[Rectangle]\
- ({self.id}){self.__x}/{self.__y} - {self.__width}/{self.__height}"
-
-    @property
-    def width(self):
-        """width getter"""
         return self.__width
 
     @width.setter
-    def width(self, val):
-        """width setter"""
-        if type(val) != int:
-            raise TypeError("width must be an integer")
-        if val < 0:
-            raise ValueError("width must be greater than zero!")
-        self.__width = val
+    def width(self, width):
+        """_summary_
 
-    """height attribute"""
+        Args:
+            width (_type_): _description_
+        """
+        self.__validate_int("width", width)
+        self.__validate_positive("width", width)
+        self.__width = width
+
     @property
     def height(self):
+        """_summary_
+
+        Returns:
+            _type_: _description_
+        """
         return self.__height
 
     @height.setter
-    def height(self, val):
-        if type(val) != int:
-            raise TypeError("height must be an integer")
-        if val < 0:
-            raise ValueError("height must be greater than zero!")
+    def height(self, height):
+        """_summary_
 
-        self.__height = val
+        Args:
+            height (_type_): _description_
+        """
+        self.__validate_int("height", height)
+        self.__validate_positive("height", height)
+        self.__height = height
 
-    """x attribute"""
     @property
     def x(self):
+        """_summary_
+
+        Returns:
+            _type_: _description_
+        """
         return self.__x
 
     @x.setter
-    def x(self, val):
-        if type(val) != int:
-            raise TypeError("x must be an integer")
-        if val < 0:
-            raise ValueError("x must be greater than zero!")
+    def x(self, x):
+        """_summary_
 
-        self.__x = val
+        Args:
+            x (_type_): _description_
+        """
+        self.__validate_int("x", x)
+        self.__validate_negative("x", x)
+        self.__x = x
 
-    """y attribute"""
     @property
     def y(self):
+        """_summary_
+
+        Returns:
+            _type_: _description_
+        """
         return self.__y
 
     @y.setter
-    def y(self, val):
-        if type(val) != int:
-            raise TypeError("y must be an integer")
-        if val < 0:
-            raise ValueError("y must be greater than zero!")
+    def y(self, y):
+        """_summary_
 
-        self.__y = val
+        Args:
+            y (_type_): _description_
+        """
+        self.__validate_int("y", y)
+        self.__validate_negative("y", y)
+        self.__y = y
 
+    def __init__(self, width, height, x=0, y=0, id=None):
+        """_summary_
+
+        Args:
+            width (_type_): _description_
+            height (_type_): _description_
+            x (int, optional): _description_. Defaults to 0.
+            y (int, optional): _description_. Defaults to 0.
+            id (_type_, optional): _description_. Defaults to None.
+        """
+        super().__init__(id)
+        self.__validate(width, height, x, y)
+        self.__width = width
+        self.__height = height
+        self.__x = x
+        self.__y = y
+
+    def __validate(self, width, height, x, y):
+        """_summary_
+
+        Args:
+        width (_type_): _description_
+        height (_type_): _description_
+        x (_type_): _description_
+        y (_type_): _description_
+        """
+        self.__validate_int("width", width)
+        self.__validate_positive("width", width)
+        self.__validate_int("height", height)
+        self.__validate_positive("height", height)
+        self.__validate_int("x", x)
+        self.__validate_negative("x", x)
+        self.__validate_int("y", y)
+        self.__validate_negative("y", y)
+
+    def __validate_int(self, name, value):
+        """
+        __validate_int
+        validates value:
+        you can assume name is always a string
+        Args:
+            name (_type_): _description_
+            value (_type_): _description_
+        """
+        if value.__class__ != int:
+            raise TypeError(f"{name} must be an integer")
+
+    def __validate_negative(self, name, value):
+        """_summary_
+
+        Args:
+            name (_type_): _description_
+            value (_type_): _description_
+
+        Raises:
+            ValueError: _description_
+        """
+        if value < 0:
+            raise ValueError(f"{name} must be >= 0")
+
+    def __validate_positive(self, name, value):
+        """
+        __validate_positive
+        """
+        if value <= 0:
+            raise ValueError(f"{name} must be > 0")
+
+    def area(self):
+        """area"""
+        return self.__width * self.__height
+
+    def display(self):
+        """display"""
+        for r in range(0, self.__height + self.__y):
+            for c in range(0, self.__width + self.__x):
+                if (r < self.__y):
+                    break
+                if (c < self.__x):
+                    print(" ", end="")
+                else:
+                    print("#", end="")
+            print()
+
+    def __str__(self):
+        """__str__"""
+        return (f"[Rectangle] ({self.id}) {self.__x}/{self.__y} - "
+                f"{self.__width}/{self.__height}")
+
+    
     def to_dictionary(self):
-        """dictionary representation """
+        """_summary_   this is very long description
+        """
         return {
-            "width": self.width,
-            "height": self.height,
-            "x": self.x,
-            "y": self.y,
-
+            'id': self.id,
+            'width': self.width,
+            'height': self.height,
+            'x': self.x,
+            'y': self.y
         }

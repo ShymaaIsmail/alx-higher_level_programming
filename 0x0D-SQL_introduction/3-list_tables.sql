a9ca6935d3bb198b29c10d49da576c92c310b605
ShymaaIsmail <shymaa.m.ismail@gmail.com>
2024-01-16 22:14:05 +0300

    Update 3-list_tables.sql

--- 0x0D-SQL_introduction/3-list_tables.sql
+++ 0x0D-SQL_introduction/3-list_tables.sql
@@ -1,2 +1,2 @@
 -- 3-list_tables.sql
-SELECT table_name FROM information_schema.tables WHERE table_type='BASE TABLE' AND table_schema='$1'
+SELECT table_name FROM information_schema.tables WHERE table_type='BASE TABLE' AND table_schema=?

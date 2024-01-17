d7737fdfbc37d9757ee7e039861bd7c8a6455902
ShymaaIsmail <shymaa.m.ismail@gmail.com>
2024-01-17 04:26:19 +0300

    Create 9-full_creation.sql

---
+++ 0x0D-SQL_introduction/9-full_creation.sql
@@ -0,0 +1,10 @@
+-- 9-full_creation.sql
+CREATE TABLE IF NOT EXISTS second_table
+(id INT,
+  name VARCHAR(256),
+  score int)
+INSERT second_table VALUES
+(1, 'John', 10),
+(2, 'Alex', 3),
+(3, 'Bob', 14),
+(4, 'George', 8)

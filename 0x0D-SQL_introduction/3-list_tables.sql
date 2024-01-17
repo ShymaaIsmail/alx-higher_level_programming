9ae6286d15a14c08f5b223ed7add1b4936496707
ShymaaIsmail <shymaa.m.ismail@gmail.com>
2024-01-17 03:36:58 +0300

    Update 3-list_tables.sql

--- 0x0D-SQL_introduction/3-list_tables.sql
+++ 0x0D-SQL_introduction/3-list_tables.sql
@@ -1,2 +1,3 @@
 -- 3-list_tables.sql
-SELECT table_name FROM information_schema.tables WHERE table_type='BASE TABLE' AND table_schema="$1"
+SCHEMA_NAME='$1' # Replace with your actual schema name
+SELECT table_name FROM information_schema.tables WHERE table_type='BASE TABLE' AND table_schema='$SCHEMA_NAME'

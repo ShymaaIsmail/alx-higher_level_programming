01adfd8899489a8d851e14ae2ef471b1faca1dc1
ShymaaIsmail <shymaa.m.ismail@gmail.com>
2024-01-17 05:20:42 +0300

    Update 100-move_to_utf8.sql

--- 0x0D-SQL_introduction/100-move_to_utf8.sql
+++ 0x0D-SQL_introduction/100-move_to_utf8.sql
@@ -1,3 +1,4 @@
 -- change db collation(db rules for how data is stored and compared)
 ALTER DATABASE CHARACTER SET utf8mb4 collate utf8mb4_unicode_ci;
-ALTER TABLE first_table MODIFY name VARCHAR(256) CHARACTER SET utf8mb4;
+ALTER TABLE first_table CONVERT TO CHARACTER SET utf8mb4 COLLATE utf8mb4_unicode_ci;
+ALTER TABLE first_table MODIFY COLUMN name VARCHAR(255) CHARACTER SET utf8mb4 COLLATE utf8mb4_unicode_ci;

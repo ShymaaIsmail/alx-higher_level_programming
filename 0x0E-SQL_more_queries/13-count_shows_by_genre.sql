d8810e5b0d7681dafaa986cb88c7cbec5e2a8edb
shymaaismail <shymaa.m.ismail@gmail.com>
2024-01-22 10:11:30 +0300

    task 13 fix

--- 0x0E-SQL_more_queries/13-count_shows_by_genre.sql
+++ 0x0E-SQL_more_queries/13-count_shows_by_genre.sql
@@ -1,8 +1,8 @@
 -- 13-count_shows_by_genre.sql
-SELECT tv_genres.name AS 'genre', COUNT(DISTINCT(tv_shows.id)) AS 'number_of_shows'
+SELECT tv_genres.name AS 'genre', COUNT(tv_shows.id) AS 'number_of_shows'
 FROM tv_shows JOIN tv_show_genres
 ON tv_shows.id = tv_show_genres.show_id
 JOIN tv_genres
 ON  tv_show_genres.genre_id = tv_genres.id
 GROUP BY tv_genres.name
-ORDER BY  COUNT(DISTINCT(tv_shows.id)) DESC
+ORDER BY  number_of_shows DESC

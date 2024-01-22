b48d3806f4e3ff5d1b12e3fe9c6e904cfd71a510
shymaaismail <shymaa.m.ismail@gmail.com>
2024-01-22 09:46:59 +0300

    fix task 10

--- 0x0E-SQL_more_queries/10-genre_id_by_show.sql
+++ 0x0E-SQL_more_queries/10-genre_id_by_show.sql
@@ -1,6 +1,6 @@
 -- 10-genre_id_by_show.sql
 SELECT tv_shows.title, tv_show_genres.genre_id
-FROM tv_shows join tv_show_genres
+FROM tv_shows JOIN tv_show_genres
 ON tv_shows.id = tv_show_genres.show_id
 JOIN tv_genres
 ON  tv_show_genres.genre_id = tv_genres.id

4a2e860964c98651dff2ebddc61e417297c93aac
shymaaismail <shymaa.m.ismail@gmail.com>
2024-02-12 11:12:14 +0300

    task 3 and 4

--- 0x12-javascript-warm_up/2-arguments.js
+++ 0x12-javascript-warm_up/2-arguments.js
@@ -2,13 +2,13 @@
 const { argv } = require('node:process');
 
 switch (argv.length) {
-    case 2:
-        console.log("No argument")
-        break;
-    case 3:
-        console.log("Argument found")
-        break;
-    default:
-        console.log("Arguments found")
-        break;
+  case 2:
+    console.log('No argument');
+    break;
+  case 3:
+    console.log('Argument found');
+    break;
+  default:
+    console.log('Arguments found');
+    break;
 }

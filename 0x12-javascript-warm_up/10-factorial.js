42bd6d013b68ae1836a6b685dec7a1fc9f1ee44c
shymaaismail <shymaa.m.ismail@gmail.com>
2024-02-12 12:04:34 +0300

    task 10

---
+++ 0x12-javascript-warm_up/10-factorial.js
@@ -0,0 +1,18 @@
+#!/usr/bin/node
+const { argv } = require('node:process');
+
+function factorial (n) {
+  let fn = 0;
+  if (n === undefined) {
+    fn = 1;
+  }
+  if (n < 0) {
+    fn = -1;
+  } else if (n === 0) {
+    fn = 1;
+  } else if (n > 0) {
+    fn = n * factorial(n - 1);
+  }
+  return (fn);
+}
+console.log(factorial(argv[2]));

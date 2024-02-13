ed55f08c9345a181c2de0ef72b05542f6dc107e5
shymaaismail <shymaa.m.ismail@gmail.com>
2024-02-13 12:49:17 +0300

    task 101

--- 0x13-javascript_objects_scopes_closures/101-sorted.js
+++ 0x13-javascript_objects_scopes_closures/101-sorted.js
@@ -1,1 +1,14 @@
 #!/usr/bin/node
+const dict = require('./101-data').dict;
+
+console.log(dict);
+const newDict = Object.fromEntries(
+  Object.entries(dict).map(([key, value]) => {
+    return [
+      value,
+      Object.entries(dict).filter(([k, val]) => val === value)
+        .map(([key, val]) => key)
+    ];
+  }
+  ));
+console.log(newDict);

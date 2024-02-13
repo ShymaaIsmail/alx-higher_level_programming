64026cff78295a342b7b734f1fb295c828f6411c
shymaaismail <shymaa.m.ismail@gmail.com>
2024-02-13 11:10:21 +0300

    task 2 and 3

--- 0x13-javascript_objects_scopes_closures/3-rectangle.js
+++ 0x13-javascript_objects_scopes_closures/3-rectangle.js
@@ -1,1 +1,25 @@
 #!/usr/bin/node
+class Rectangle {
+  constructor (w, h) {
+    if (w > 0 && h > 0) {
+      this.width = w;
+      this.height = h;
+    }
+  }
+
+  print () {
+    let rect = '';
+    for (let index = 0; index < this.height; index++) {
+      for (let index = 0; index < this.width; index++) {
+        rect += 'X';
+      }
+      if (index < this.height - 1) {
+        rect += '\n';
+      }
+    }
+    if (rect.length > 0) {
+      console.log(rect);
+    }
+  }
+}
+module.exports = Rectangle;

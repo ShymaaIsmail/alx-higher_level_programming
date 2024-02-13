3aea99bb82997ce1bb0fe253efda656298a1c251
shymaaismail <shymaa.m.ismail@gmail.com>
2024-02-13 12:55:53 +0300

    task 102

--- 0x13-javascript_objects_scopes_closures/102-concat.js
+++ 0x13-javascript_objects_scopes_closures/102-concat.js
@@ -1,1 +1,30 @@
 #!/usr/bin/node
+const fs = require('fs');
+
+const fileA = process.argv[2];
+const fileB = process.argv[3];
+const fileC = process.argv[4];
+fs.readFile(fileA, 'utf8', (err, data1) => {
+  if (err) {
+    console.error('Error reading file1:', err);
+    return;
+  }
+
+  // Read content of file2
+  fs.readFile(fileB, 'utf8', (err, data2) => {
+    if (err) {
+      console.error('Error reading file2:', err);
+      return;
+    }
+
+    // Combine content of file1 and file2
+    const combinedContent = data1 + data2;
+
+    // Write combined content to a new file
+    fs.writeFile(fileC, combinedContent, (err) => {
+      if (err) {
+
+      }
+    });
+  });
+});

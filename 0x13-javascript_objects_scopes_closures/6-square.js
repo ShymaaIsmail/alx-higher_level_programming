3f34e17e4cd7ff3f6101f0fe191957a62388eac2
shymaaismail <shymaa.m.ismail@gmail.com>
2024-02-13 11:27:40 +0300

    task 6

--- 0x13-javascript_objects_scopes_closures/6-square.js
+++ 0x13-javascript_objects_scopes_closures/6-square.js
@@ -1,1 +1,24 @@
 #!/usr/bin/node
+const parentSquare = require('./5-square');
+
+class Square extends parentSquare {
+  charPrint (c) {
+    if (c) {
+      let rect = '';
+      for (let index = 0; index < this.height; index++) {
+        for (let index = 0; index < this.width; index++) {
+          rect += c;
+        }
+        if (index < this.height - 1) {
+          rect += '\n';
+        }
+      }
+      if (rect.length > 0) {
+        console.log(rect);
+      }
+    } else {
+      this.print();
+    }
+  }
+}
+module.exports = Square;

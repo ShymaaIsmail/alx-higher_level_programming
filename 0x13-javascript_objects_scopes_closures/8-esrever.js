2fc04b7f98bf835927e2b7dc1f112f2885ebb123
shymaaismail <shymaa.m.ismail@gmail.com>
2024-02-13 11:48:01 +0300

    task 8

--- 0x13-javascript_objects_scopes_closures/8-esrever.js
+++ 0x13-javascript_objects_scopes_closures/8-esrever.js
@@ -1,1 +1,11 @@
 #!/usr/bin/node
+exports.esrever = function (list) {
+  const revList = [];
+  if (list) {
+    for (let index = list.length - 1; index >= 0; index--) {
+      const element = list[index];
+      revList.push(element);
+    }
+  }
+  return revList;
+};

fe6cef651b32604030a6513659c5e43ed2513e1d
shymaaismail <shymaa.m.ismail@gmail.com>
2024-02-15 11:44:06 +0300

    fix styles

--- 0x0F-python-object_relational_mapping/model_state.py
+++ 0x0F-python-object_relational_mapping/model_state.py
@@ -2,11 +2,12 @@
 """City Model"""
 from sqlalchemy.ext.declarative import declarative_base
 from sqlalchemy import Column, Integer, String
-
 Base = declarative_base()
+
+
 class State(Base):
     """State Model"""
     __tablename__ = "states"
     id = Column(Integer, primary_key=True, nullable=False, unique=True,
                 autoincrement=True)
-    name =  Column(String(128),nullable=False)
+    name = Column(String(128), nullable=False)

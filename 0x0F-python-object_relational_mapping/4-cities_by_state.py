3d00202d026697d21da85f93b65ae4edf9c53242
shymaaismail <shymaa.m.ismail@gmail.com>
2024-02-15 10:52:08 +0300

    task 4

--- 0x0F-python-object_relational_mapping/4-cities_by_state.py
+++ 0x0F-python-object_relational_mapping/4-cities_by_state.py
@@ -1,1 +1,28 @@
 #!/usr/bin/python3
+""" SELECT ALL cities Module"""
+import sys
+import MySQLdb
+
+
+def select_cities():
+    """SELECT ALL cities"""
+    host = "localhost"
+    port = 3306
+    user_name = sys.argv[1]
+    password = sys.argv[2]
+    database_name = sys.argv[3]
+    conn = MySQLdb.connect(host=host, port=port, user=user_name,
+                           passwd=password, db=database_name, charset="utf8")
+    cur = conn.cursor()
+    cur.execute("SELECT cities.id, cities.name, states.name "
+                "FROM cities JOIN states ON cities.state_id = states.id "
+                "ORDER BY cities.id ASC")
+    query_rows = cur.fetchall()
+    for row in query_rows:
+        print(row)
+    cur.close()
+    conn.close()
+
+
+if __name__ == "__main__":
+    select_cities()

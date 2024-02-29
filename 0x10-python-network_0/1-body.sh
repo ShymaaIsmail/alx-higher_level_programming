8e464de64db341610c8d9c23a2958c6c77a1a676
shymaaismail <shymaa.m.ismail@gmail.com>
2024-03-01 00:03:49 +0300

    f

--- 0x10-python-network_0/1-body.sh
+++ 0x10-python-network_0/1-body.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 # print response size 
-response=$(curl  -s  -w "%{http_code}"  "$1")
+response=$(curl -X GET -s  -w "%{http_code}"  "$1")
 status="${response: -3}"
 if [ "$status" = "200" ]; then
     response_body=$(echo "$response" | sed '$s/...$//')

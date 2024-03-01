de2e8ee7c6969629441f946113afc100d8151f2e
shymaaismail <shymaa.m.ismail@gmail.com>
2024-03-01 11:39:25 +0300

    taask 1

--- 0x10-python-network_0/1-body.sh
+++ 0x10-python-network_0/1-body.sh
@@ -1,8 +1,3 @@
 #!/bin/bash
-# print response size 
-response=$(curl -X GET -s  -w "%{http_code}"  "$1")
-status="${response: -3}"
-if [ "$status" = "200" ]; then
-    response_body=$(echo "$response" | sed '$s/...$//')
-    echo "$response_body"
-fi
+# print response size
+curl -s -w "%{http_code}" -o response.txt "$1" | grep -q "^200$" && cat response.txt

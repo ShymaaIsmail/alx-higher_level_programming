74cd4a08e3086bc0310818707af6f70c1cc8fe16
shymaaismail <shymaa.m.ismail@gmail.com>
2024-03-02 16:52:46 +0300

    task 1

--- 0x11-python-network_1/1-hbtn_header.py
+++ 0x11-python-network_1/1-hbtn_header.py
@@ -1,1 +1,10 @@
 #!/usr/bin/python3
+""" get header"""
+
+
+if __name__ == "__main__":
+    import urllib.request
+    import sys
+
+    with urllib.request.urlopen(sys.argv[1]) as response:
+        print(response.headers["X-Request-Id"])

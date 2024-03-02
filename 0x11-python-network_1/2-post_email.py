822972a144734552a4988e05a75aaa3947beaa13
shymaaismail <shymaa.m.ismail@gmail.com>
2024-03-02 17:03:54 +0300

    task2

--- 0x11-python-network_1/2-post_email.py
+++ 0x11-python-network_1/2-post_email.py
@@ -1,1 +1,14 @@
 #!/usr/bin/python3
+""" post email"""
+
+
+if __name__ == "__main__":
+    import urllib.request
+    import sys
+
+    data = {'email': sys.argv[2]}
+    request = {'method': 'POST', 'url': sys.argv[1], 'data': data}
+    with urllib.request.urlopen(request) as response:
+        content = response.read()
+        print({content.decode('utf-8')})
+

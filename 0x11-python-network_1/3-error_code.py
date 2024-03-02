c045fab0e39652074f080f89cdbdb114c12fc436
shymaaismail <shymaa.m.ismail@gmail.com>
2024-03-02 17:14:57 +0300

    3

--- 0x11-python-network_1/3-error_code.py
+++ 0x11-python-network_1/3-error_code.py
@@ -1,1 +1,11 @@
 #!/usr/bin/python3
+""" get status"""
+
+
+if __name__ == "__main__":
+    import urllib.request
+    import sys
+
+    with urllib.request.urlopen(sys.argv[1]) as response:
+        status = response.read()
+        print(f"Error code: {status.decode('utf-8')}")

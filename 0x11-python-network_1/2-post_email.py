9eeed14fc1ddea4fe387f7b3a82d90263356a613
shymaaismail <shymaa.m.ismail@gmail.com>
2024-03-02 17:10:53 +0300

    urllib.parse

--- 0x11-python-network_1/2-post_email.py
+++ 0x11-python-network_1/2-post_email.py
@@ -4,9 +4,11 @@
 
 if __name__ == "__main__":
     import urllib.request
+    import urllib.parse
     import sys
 
     data = {'email': sys.argv[2]}
+    data = urllib.parse.urlencode(data)
     data = data.encode('ascii')
     request = urllib.request.Request(sys.argv[1], data)
     with urllib.request.urlopen(request) as response:

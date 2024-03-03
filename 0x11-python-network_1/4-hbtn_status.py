c6868abe180654fe4eb20299ca4604e9bdb530d6
shymaaismail <shymaa.m.ismail@gmail.com>
2024-03-03 08:20:19 +0300

    Get status code and content from the URL. (generated by blackbox.ai )

--- 0x11-python-network_1/4-hbtn_status.py
+++ 0x11-python-network_1/4-hbtn_status.py
@@ -1,1 +1,12 @@
 #!/usr/bin/python3
+""" get status"""
+
+
+if __name__ == "__main__":
+    import requests
+
+    with requests.get('https://alx-intranet.hbtn.io/'
+                        'status') as response:
+        print("Body response:")
+        print(f"\t- type: {type(response.text)}")
+        print(f"\t- content: {response.text}")

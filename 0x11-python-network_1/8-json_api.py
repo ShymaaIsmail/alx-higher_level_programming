a977012b931e020303b2511b8b5ba6557a09c785
shymaaismail <shymaa.m.ismail@gmail.com>
2024-03-03 08:55:45 +0300

    89

--- 0x11-python-network_1/8-json_api.py
+++ 0x11-python-network_1/8-json_api.py
@@ -6,7 +6,8 @@ if __name__ == "__main__":
     import requests
     import sys
 
-    querystring = f"q={sys.argv[2]}" if len(sys.argv) > 2 else "q="
-    with requests.post(f"http://0.0.0.0:5000/search_user?{querystring}") as response:
+    querystring = f"q={sys.argv[1]}" if len(sys.argv) >= 2 else "q="
+    with requests.post(f"http://0.0.0.0:5000/search_user"
+                       "?{querystring}") as response:
         content = response.text
         print(content)

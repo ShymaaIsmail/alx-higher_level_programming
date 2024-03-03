8e492091116561412b3ae1586289cbfbb15de8c6
shymaaismail <shymaa.m.ismail@gmail.com>
2024-03-03 09:58:31 +0300

    100

--- 0x11-python-network_1/100-github_commits.py
+++ 0x11-python-network_1/100-github_commits.py
@@ -1,1 +1,17 @@
 #!/usr/bin/python3
+""" get github last 10 commits"""
+
+
+if __name__ == "__main__":
+    import requests
+    import sys
+
+    headers = {"Accept": "application/vnd.github.VERSION.sha"}
+    with requests.get(f"https://api.github.com/repos/"
+                      "{sys.argv[2]}/{sys.argv[1]}/commits/master",
+                      headers=headers) as response:
+        try:
+            commits = response.json()
+            print(commits)
+        except Exception:
+            print("None")

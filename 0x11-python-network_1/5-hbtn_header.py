27056d6c8e8a9e455ef23b09644ae87d0949fe18
shymaaismail <shymaa.m.ismail@gmail.com>
2024-03-03 08:23:11 +0300

    5

--- 0x11-python-network_1/5-hbtn_header.py
+++ 0x11-python-network_1/5-hbtn_header.py
@@ -1,1 +1,10 @@
 #!/usr/bin/python3
+""" get header"""
+
+
+if __name__ == "__main__":
+    import requests
+    import sys
+
+    with requests.get(sys.argv[1]) as response:
+        print(response.headers["X-Request-Id"])

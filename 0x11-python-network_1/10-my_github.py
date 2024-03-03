e2177fef4e710abbfa335a4e5fa8a80092538847
shymaaismail <shymaa.m.ismail@gmail.com>
2024-03-03 09:30:23 +0300

    10

--- 0x11-python-network_1/10-my_github.py
+++ 0x11-python-network_1/10-my_github.py
@@ -1,1 +1,18 @@
 #!/usr/bin/python3
+""" get my github id"""
+
+
+if __name__ == "__main__":
+    import requests
+    import sys
+
+    github_headers = {"Accept": "application/vnd.github+json",
+                      "Authorization": f"Bearer {sys.argv[2]}",
+                      "X-GitHub-Api-Version": "2022-11-28"}
+    with requests.get(f"https://api.github.com/users/{sys.argv[1]}",
+                      headers=github_headers) as response:
+        try:
+            profile_data = response.json()
+            print(profile_data["id"])
+        except Exception:
+            print("None")

b644bbfba03125c09a6b2147292ed3f7a4ef0d80
shymaaismail <shymaa.m.ismail@gmail.com>
2024-03-03 08:35:20 +0300

    7

--- 0x11-python-network_1/7-error_code.py
+++ 0x11-python-network_1/7-error_code.py
@@ -1,1 +1,14 @@
 #!/usr/bin/python3
+""" get status"""
+
+
+if __name__ == "__main__":
+    import requests
+    import sys
+
+    try:
+        with requests.get(sys.argv[1]) as response:
+            status = response.text
+            print(status)
+    except requests.exceptions.HTTPError as e:
+        print(f"Error code: {e}")

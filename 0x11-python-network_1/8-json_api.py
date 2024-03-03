c2a24c8dc2709faa4ebd820b2fb768ed291518ab
shymaaismail <shymaa.m.ismail@gmail.com>
2024-03-03 08:44:13 +0300

    8

--- 0x11-python-network_1/8-json_api.py
+++ 0x11-python-network_1/8-json_api.py
@@ -1,1 +1,13 @@
 #!/usr/bin/python3
+""" post email"""
+
+
+if __name__ == "__main__":
+    import requests
+    import sys
+
+    querystring = f"q={sys.argv[2]}" if len(sys.argv) > 2 else "q="
+    with requests.post("http://0.0.0.0:5000/search_user",
+                       querystring) as response:
+        content = response.text
+        print(content)

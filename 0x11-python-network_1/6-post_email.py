06dc0d44b86ba7ded9e03d6d1d0622b87f21f692
shymaaismail <shymaa.m.ismail@gmail.com>
2024-03-03 08:31:47 +0300

    6

--- 0x11-python-network_1/6-post_email.py
+++ 0x11-python-network_1/6-post_email.py
@@ -1,1 +1,12 @@
 #!/usr/bin/python3
+""" post email"""
+
+
+if __name__ == "__main__":
+    import requests
+    import sys
+
+    data = {'email': sys.argv[2]}
+    with requests.post(sys.argv[1], data) as response:
+        content = response.text
+        print(content.decode('utf-8'))

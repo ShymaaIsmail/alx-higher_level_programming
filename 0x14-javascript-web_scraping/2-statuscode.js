587249d9e46084f7750705aa6f316c38e3a0bc1c
shymaaismail <shymaa.m.ismail@gmail.com>
2024-03-27 13:41:00 +0300

    fix

--- 0x14-javascript-web_scraping/2-statuscode.js
+++ 0x14-javascript-web_scraping/2-statuscode.js
@@ -1,1 +1,6 @@
 #!/usr/bin/node
+const http = require('request');
+http.get(process.argv[2], function (error, response, body) {
+  if (error) { console.log(error); }
+  console.log('Code: ' + response.statusCode);
+});

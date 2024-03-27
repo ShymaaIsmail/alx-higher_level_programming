2563e5ea472467fdbc23ad39f75c2f6cd38429e8
shymaaismail <shymaa.m.ismail@gmail.com>
2024-03-28 01:10:15 +0300

    tasjk4

--- 0x14-javascript-web_scraping/4-starwars_count.js
+++ 0x14-javascript-web_scraping/4-starwars_count.js
@@ -1,1 +1,12 @@
 #!/usr/bin/node
+const http = require('request');
+const url = process.argv[2];
+http.get(url, function (error, response, body) {
+  if (error) {
+    console.error(error);
+    return;
+  }
+  const result = JSON.parse(body);
+  const movies = result.results.filter(a => a.characters.includes('https://swapi-api.alx-tools.com/api/people/18/'));
+  console.log(movies.length);
+});

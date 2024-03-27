47456fa699feb262154f948d3fb2f3aa8d767f68
shymaaismail <shymaa.m.ismail@gmail.com>
2024-03-28 01:18:20 +0300

    task 5

--- 0x14-javascript-web_scraping/5-request_store.js
+++ 0x14-javascript-web_scraping/5-request_store.js
@@ -1,1 +1,22 @@
 #!/usr/bin/node
+const request = require('request');
+const fs = require('fs');
+
+const url = process.argv[2];
+const filePath = process.argv[3];
+
+request.get({ url, encoding: 'utf-8' }, function (error, response, body) {
+  if (error) {
+    console.error('Error fetching the webpage:', error);
+    return;
+  }
+
+  // Write the body content to the file
+  fs.writeFile(filePath, body, 'utf-8', function (err) {
+    if (err) {
+      console.error('Error writing to file:', err);
+      return;
+    }
+    console.log('Webpage content has been saved to:', filePath);
+  });
+});

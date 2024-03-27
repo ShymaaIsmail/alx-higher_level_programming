032f635d5d19a24aac5d2d5849d8d6b0ae065f6d
shymaaismail <shymaa.m.ismail@gmail.com>
2024-03-28 01:16:07 +0300

    d

--- 0x14-javascript-web_scraping/4-starwars_count.js
+++ 0x14-javascript-web_scraping/4-starwars_count.js
@@ -7,9 +7,8 @@ http.get(url, function (error, response, body) {
     return;
   }
   const result = JSON.parse(body);
-  const movies = result.results.filter(a => 
-    {
-        return a.characters.some(character => character.endsWith('/18/'));
-    });
+  const movies = result.results.filter(a => {
+    return a.characters.some(character => character.endsWith('/18/'));
+  });
   console.log(movies.length);
 });

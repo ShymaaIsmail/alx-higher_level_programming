f433a8a13f52187d5acb2ec3d5040c7522065bed
shymaaismail <shymaa.m.ismail@gmail.com>
2024-03-27 13:46:03 +0300

    f

--- 0x14-javascript-web_scraping/2-statuscode.js
+++ 0x14-javascript-web_scraping/2-statuscode.js
@@ -2,7 +2,7 @@
 const http = require('request');
 http.get(process.argv[2], function (error, response) {
   if (error) {
-    console.log(error);
+    console.error(error);
     return;
   }
   console.log('Code: ' + response.statusCode);
